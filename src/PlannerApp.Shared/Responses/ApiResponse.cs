d1aa1d7bdf8b44a5ab7945e5014d2185d90a0081
Phalgun <phalgunarao.b@gmail.com>
2021-05-31 16:21:32 +0530

    Create the Api Response and Register and Ini HttpClient

---
+++ src/PlannerApp.Shared/Responses/ApiResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Responses
+{
+    public class ApiResponse
+    {
+        public string Message { get; set; }
+        public bool IsSucess { get; set; }
+
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T Value { get; set; }
+    }
+}
+
+public class ApiErrorResponse
+{
+    public string Message { get; set; }
+    public bool IsSuccess { get; set; }
+    public string[] Errors { get; set; }
+    public Userinfo UserInfo { get; set; }
+    public DateTime ExpireDate { get; set; }
+}
+
+public class Userinfo
+{
+    public string AdditionalProp1 { get; set; }
+    public string AdditionalProp2 { get; set; }
+    public string AdditionalProp3 { get; set; }
+}
